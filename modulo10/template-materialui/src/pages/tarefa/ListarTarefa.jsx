b92998b1f419fb97138a4f1871af43b62956027f
Francisco Thiago <vieirathiago779@gmail.com>
2024-12-08 23:28:49 -0300

    dark theme in the list component

--- modulo10/template-materialui/src/pages/tarefa/ListarTarefa.jsx
+++ modulo10/template-materialui/src/pages/tarefa/ListarTarefa.jsx
@@ -35,11 +35,13 @@ function createData(
 const initialRows = [
   createData(1, 'Tarefa 1', 'Descrição da Tarefa 1', '2022-01-01', '2022-01-02', 'Concluída', 'Recurso 1'),
   createData(2, 'Tarefa 2', 'Descrição da Tarefa 2', '2022-01-03', '2022-01-04', 'Em Andamento', 'Recurso 2'),
-  createData(3, 'Tarefa 3', 'Descrição da Tarefa 3', '2022-01-04', '2022-01-05', 'Em Andamento', 'Recurso 3'),
-  createData(4, 'Tarefa 4', 'Descrição da Tarefa 4', '2022-01-05', '2022-01-06', 'Em Andamento', 'Recurso 4'),
-  createData(5, 'Tarefa 5', 'Descrição da Tarefa 5', '2022-01-06', '2022-01-07', 'Em Andamento', 'Recurso 5'),
-  createData(6, 'Tarefa 6', 'Descrição da Tarefa 6', '2022-01-07', '2022-01-08', 'Aguardando', 'Recurso 6'),
 ];
+
+const darkTheme = {
+  color: "white",
+  backgroundColor: "#1e1e1e",
+  padding: "1em",
+};
 
 //Componente ListarTarefa
 const ListarTarefa = () => {
@@ -83,10 +85,9 @@ const ListarTarefa = () => {
 
     return(
     <>
-    <Card>
+    <Card style={darkTheme}>
         <CardHeader
           title="Tarefas"
-          subheader="Listagem de Tarefas"
         /> 
         <CardContent>
             <TableContainer component={Paper}>
